d22d52fe16352629d71b0e669446aafe3adfb3b7
Eileen McNaughton <eileen@fuzion.co.nz>
2014-01-08 21:59:10 +1300

    2158827 New Baseline based on code changes over previous year - now works with apiv3 see ticket for more detail of changes

--- uc_civicrm.js
+++
@@ -1,9 +0,0 @@
-Drupal.behaviors.uccivicrmBehavior = function (context) {
-  $('#edit-uc-civicrm-location-update').click(function() {
-    if ($(this).attr('checked')) {
-      $('#edit-uc-civicrm-location-type').attr('disabled', true);
-    } else {
-      $('#edit-uc-civicrm-location-type').removeAttr('disabled');
-    }
-  });
-}
